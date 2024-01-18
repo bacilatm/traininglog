d54aa5a2c3a28eb18445a2f6d90ec1c144055a63
ioantalos <ioan.talos@sita.aero>
2024-01-18 16:24:46 +0200

    clean up

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,7 @@
 import { Routes } from '@angular/router';
 import { TrainingsDashboardComponent } from './trainings-dashboard/trainings-dashboard.component';
-import { AppComponent } from './app.component';
 
 export const routes: Routes = [
     { path: 'traininglog', component: TrainingsDashboardComponent },
-    { path: 'home', component: AppComponent },
-    { path: '', redirectTo: '/home', pathMatch: 'full' }
+    { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
