ab1c5d79c9343b09320ed6c14d6df3b5c7e8e461
bacilatm <mihaibacila69@gmail.com>
2024-01-20 17:08:28 +0200

    Navigation Menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink, RouterOutlet} from '@angular/router';
+import { AppNavComponent } from "./app-nav/app-nav.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
-  imports: [CommonModule, RouterLink, RouterOutlet]
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    imports: [CommonModule, RouterLink, RouterOutlet, AppNavComponent]
 })
 
 export class AppComponent {
