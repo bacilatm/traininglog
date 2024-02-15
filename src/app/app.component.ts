02930285d78dcd893d8be8feec5cdcefddc181c1
bacilatm <136748383+bacilatm@users.noreply.github.com>
2024-02-15 20:16:33 +0200

    Navigation Menu (#3)
    * Navigation Menu
    
    * Required modifications done.
    
    * Required modifications done.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink, RouterOutlet} from '@angular/router';
+import { MainComponent } from "./main/main.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
-  imports: [CommonModule, RouterLink, RouterOutlet]
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    imports: [CommonModule, RouterLink, RouterOutlet, MainComponent]
 })
 
 export class AppComponent {
---
+++ src/app/main/main.component.ts
@@ -0,0 +1,41 @@
+import { Component, inject } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { AsyncPipe } from '@angular/common';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { ToolbarComponent } from "../toolbar-component/toolbar.component";
+import { SideNavComponent } from '../sidenav-component/sidenav.component';
+
+@Component({
+    selector: 'app-nav',
+    templateUrl: './main.component.html',
+    styleUrl: './main.component.scss',
+    standalone: true,
+    imports: [
+        MatToolbarModule,
+        MatButtonModule,
+        MatSidenavModule,
+        MatListModule,
+        MatIconModule,
+        AsyncPipe,
+        RouterLink,
+        RouterOutlet,
+        ToolbarComponent,
+        SideNavComponent
+    ]
+})
+export class MainComponent {
+  private breakpointObserver = inject(BreakpointObserver);
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+}
