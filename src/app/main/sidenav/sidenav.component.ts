1a047fc303744a1ff3a84da97ef9b0dec6f49db5
bacilatm <mihaibacila69@gmail.com>
2024-02-27 16:22:37 +0200

    Required changes made

--- src/app/main/sidenav/sidenav.component.ts
+++ src/app/main/sidenav/sidenav.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListItem, MatNavList } from '@angular/material/list';
+import { MatMenu, MatMenuItem, MatMenuTrigger } from '@angular/material/menu';
+import { MatToolbar } from '@angular/material/toolbar';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-sidenav-component',
   standalone: true,
-  imports: [ MatSidenavModule],
+  imports: [ MatSidenavModule, MatListItem, MatMenu, MatMenuItem, MatNavList, MatToolbar, RouterLink, MatMenuTrigger],
   templateUrl: './sidenav.component.html',
   styleUrl: './sidenav.component.scss'
 })
