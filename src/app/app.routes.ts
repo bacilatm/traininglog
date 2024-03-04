f7b7b00764b4903a38f3b18fcab570d76e9d6688
bacilatm <mihaibacila69@gmail.com>
2024-03-04 11:12:45 +0200

    Add training component with chronometer and some chronometer functionalities

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { TrainingsDashboardComponent } from './main/trainings-dashboard/trainings-dashboard.component';
+import { AddTrainingComponent } from './main/add-trainings/add-training/add-training.component';
 
 export const routes: Routes = [
     { path: 'dashboard', component: TrainingsDashboardComponent },
+    { path: 'add-training', component: AddTrainingComponent },
     { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
