0aeca2cfcea02e0959747ecf7b4d1370b751541a
bacilatm <136748383+bacilatm@users.noreply.github.com>
2024-03-07 09:41:57 +0200

    Add training component (#6)
    * Add training component with chronometer and some chronometer functionalities
    
    * Changed the name of the stop button and chronometer methods

---
+++ src/app/main/add-trainings/add-training/add-training.component.ts
@@ -0,0 +1,69 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Subscription, interval } from 'rxjs';
+import { MatCardModule } from '@angular/material/card'; 
+import { MatButtonModule } from '@angular/material/button';
+
+
+@Component({
+  selector: 'app-add-training',
+  standalone: true,
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatButtonModule,
+  
+  ],
+  templateUrl: './add-training.component.html',
+  styleUrl: './add-training.component.scss'
+})
+export class AddTrainingComponent implements OnInit, OnDestroy {
+  elapsedTime: number = 0;
+  private subscription!: Subscription;
+  isPaused: boolean = false;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.startStopWatch();
+  }
+
+  ngOnDestroy(): void {
+    this.cancelTraining();
+  }
+
+  startStopWatch(): void{
+    this.subscription = interval(1000).subscribe(() => {
+      ++this.elapsedTime;
+    });
+  }
+
+  pauseStopWatch(event: MouseEvent): void {
+    if (this.isPaused) {
+      this.isPaused = false;
+      this.startStopWatch();
+    } else {
+      this.isPaused = true;
+      if (this.subscription) {
+        this.subscription.unsubscribe();
+      }
+    }
+  }
+
+  cancelTraining(): void {
+    this.subscription.unsubscribe();
+    this.elapsedTime = 0;
+  }
+
+  formatTime(seconds: number): string {
+    const hours: number = Math.floor(seconds / 3600);
+    const minutes: number = Math.floor((seconds % 3600) / 60);
+    const secs: number = seconds % 60;
+
+    return `${this.pad(hours)}:${this.pad(minutes)}:${this.pad(secs)}`;
+  }
+
+  private pad(value: number): string {
+    return value < 10 ? `0${value}` : `${value}`;
+  }
+}
